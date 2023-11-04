cf47eee121420ce365c4bdda4dbc79c0a5f44c01
matt530 <matt@spec-trust.com>
2023-11-03 18:16:26 -0700

    rearrange social icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,14 +23,14 @@ export default async function Home() {
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://instagram.com/matt.kharrl/"
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
               href="https://linkedin.com/in/mattkharrl/"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
+            />
+            <SocialLink
+              href="https://instagram.com/matt.kharrl/"
+              aria-label="Follow on Instagram"
+              icon={InstagramIcon}
             />
             <SocialLink
               href="https://github.com/colbenkharrl/"
