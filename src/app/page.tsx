9cbab97a2b723318c1185129a64dbd7871ae7ebc
matt530 <matt@spec-trust.com>
2023-11-05 20:25:23 -0700

    update themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,6 @@ import {
 } from '@/components/Icons';
 import { Photos } from '@/components/Photos';
 import { SocialLink } from '@/components/SocialLink';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Home',
-  description: 'I’m Matt Kharrl: engineer, athlete, and dad.',
-};
 
 export default async function Home() {
   return (
@@ -20,7 +14,11 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Engineer. Athlete. Dad.
+            Engineer
+            <span className="text-orange-500 dark:text-orange-400">.</span>{' '}
+            Athlete
+            <span className="text-orange-500">.</span> Dad
+            <span className="text-orange-500">.</span>
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             At any given moment I am either developing software, racing my bike,
