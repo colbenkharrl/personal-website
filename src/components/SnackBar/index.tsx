ba89518af6d9be9365222f40e8869dea8886824a
Matt Kharrl <colbenkharrl@gmail.com>
2023-11-11 12:38:57 -0700

    Bug - SnackBar Z-Index Fix (#5)
    * add image and testing to readme
    
    * update snackbar style to always be banner
    
    * fix bug snackbar with z-index overlap on portfolio page

--- src/components/SnackBar/index.tsx
+++ src/components/SnackBar/index.tsx
@@ -31,7 +31,7 @@ export function SnackBar({ onClose, children }: SnackBarProps) {
   }, [handleClose]);
 
   return isOpen ? (
-    <div className="fixed inset-x-0 bottom-0" data-testid="SnackBar">
+    <div className="fixed inset-x-0 bottom-0  z-50" data-testid="SnackBar">
       <div
         className={`${
           isCloseAnimation ? 'opacity-0' : 'opacity-100'
