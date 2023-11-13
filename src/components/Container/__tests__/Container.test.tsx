b381aed68f56252e532c07a6fabed70b3695f485
Matt Kharrl <colbenkharrl@gmail.com>
2023-11-12 21:32:33 -0700

    Add Container stories

--- src/components/Container/__tests__/Container.test.tsx
+++ src/components/Container/__tests__/Container.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { Container } from '../';
+import meta from '../__stories__/Container.stories';
 
 it('Container matches snapshot (regression test)', () => {
-  render(<Container>Contents</Container>);
+  render(<Container {...meta.args} />);
 
-  screen.getByText('Contents');
+  screen.getByText('This is some content.');
 
   expect(screen.getByTestId('Container')).toMatchSnapshot(
     'Full component snapshot.',
