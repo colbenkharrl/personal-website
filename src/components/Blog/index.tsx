b9a21394b7856661954a69aea224784044cec4f3
Matt Kharrl <colbenkharrl@gmail.com>
2023-11-12 20:30:37 -0700

    add blog stories

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -2,7 +2,12 @@ import { type BlogWithSlug } from '@/util/blogs';
 import { Card } from '@/components/Card';
 import { formatDate } from '@/util/formatDate';
 
-export type BlogProps = { blog: BlogWithSlug };
+export type BlogProps = {
+  /**
+   * Blog object specifying component content.
+   */
+  blog: BlogWithSlug;
+};
 
 export function Blog({ blog }: BlogProps) {
   return (
