9c52d24339dbf4cbe394f348516c003e16e4eaeb
Matt Kharrl <matt@catio.tech>
2024-08-09 15:22:35 -0700

    remove references to other website urls

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,14 +2,4 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://matt.kharrl.com',
     generateRobotsTxt: true,
-    alternateRefs: [
-        {
-            href: 'https://kharrl.com',
-            hrefLang: 'en'
-        },
-        {
-            href: 'https://mattkharrl.com',
-            hrefLang: 'en'
-        }
-    ],
 }
